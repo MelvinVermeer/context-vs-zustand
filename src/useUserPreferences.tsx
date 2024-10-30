7f097c2884173e6781c3dbac568414bdde8a8ad4
Melvin Vermeer <melvin.vermeer@gmail.com>
2024-10-30 09:50:46 +0100

    types

--- src/useUserPreferences.tsx
+++ src/useUserPreferences.tsx
@@ -1,11 +1,6 @@
 import { create } from "zustand";
 
-export const useUserPreferences = create<{
-  theme: "light" | "dark";
-  language: "nl" | "en";
-  setTheme: (theme: "light" | "dark") => void;
-  setLanguage: (language: "nl" | "en") => void;
-}>((set) => ({
+export const useUserPreferences = create<UserPreferencesContext>((set) => ({
   theme: "light",
   language: "nl",
   setTheme: (theme) => set({ theme }),
